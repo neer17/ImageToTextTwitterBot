4b2317021542af951ebf70e37cb9f9984571eb82
neer17 <neerajsewanisrt8@gmail.com>
2020-06-21 16:19:49 +0530

    feat: getting image url, converting to text success

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const {convertImageToText} = require('./utils/imageToText')
+
 
 const app = express()
 const port = 3000
@@ -15,9 +17,12 @@ app.get('/', (req, res) => {
 app.post('/downloadUrl', (req, res) => {
     const body = req.body
     console.info(body)
+
+    //
+    convertImageToText(body.url)
     res.json({
         "response": "Successfully got the uri"
     })
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
